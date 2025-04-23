c6c91c54e07afaddf7e98842000b0d2adc62b5e1
Ignacio Baez <ibaezg3@gmail.com>
2025-04-23 16:48:51 -0400

    Trabajo finalizado

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("h1").textContent = "Goodbye!";
+
+document.querySelector("h5").style.color = "orange"
+
+document.querySelector("h2").addEventListener("click", () => {
+    document.querySelector("h2").style.color = "brown"
+})
